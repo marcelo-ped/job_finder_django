83a601461c27efc3e1024b06504320b79f334995
marcelo-ped <marcelo.bp01@gmail.com>
2025-06-10 01:08:31 -0300

    fixing view to persist job description on db after post it and fixing docker-compose.yml to persist db after exiting container

--- job_finder/views.py
+++ job_finder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Jobs
 
 def index(request):
@@ -11,8 +11,23 @@ def index(request):
 
 def add_job(request):
     if request.method == 'POST':
-        # Process form
-        pass
+        title = request.POST['title']
+        description = request.POST['description']
+        company = request.POST['company']
+        email = request.POST['email']
+        salary = request.POST['salary']
+        new_job = request.POST.get('new_job', 0)
+
+        Jobs.objects.create(
+            title=title,
+            description=description,
+            company=company,
+            email=email,
+            salary=salary,
+            new_job=new_job
+        )
+
+        return redirect('/')
     return render(request, 'add.html')
 
 def view_job(request, job_id):
